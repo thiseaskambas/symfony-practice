10bd32d05c41eee5fd153a6a1ccfcdf8b2f3d39a
thiseaskambas <tkambas@yahoo.com>
2023-03-21 12:19:43 -0400

    templating

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -2,15 +2,62 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use function Symfony\Component\String\u;
 
-class VinylController
+class VinylController extends AbstractController
 {
     #[Route('/')]
     public function homepage():Response{
-        return new Response('a simple message');
+
+        $tracks = [
+            [
+                "song" => "Smells Like Teen Spirit",
+                "artist" => "Nirvana"
+            ],
+            [
+                "song" => "Baby One More Time",
+                "artist" => "Britney Spears"
+            ],
+            [
+                "song" => "My Heart Will Go On",
+                "artist" => "Celine Dion"
+            ],
+            [
+                "song" => "Nothing Compares 2 U",
+                "artist" => "Sinead O'Connor"
+            ],
+            [
+                "song" => "Wannabe",
+                "artist" => "Spice Girls"
+            ],
+            [
+                "song" => "Jeremy",
+                "artist" => "Pearl Jam"
+            ],
+            [
+                "song" => "Waterfalls",
+                "artist" => "TLC"
+            ],
+            [
+                "song" => "Gangsta's Paradise",
+                "artist" => "Coolio ft. L.V."
+            ],
+            [
+                "song" => "I Will Always Love You",
+                "artist" => "Whitney Houston"
+            ],
+            [
+                "song" => "Loser",
+                "artist" => "Beck"
+            ]
+        ];
+        return $this->render('vinyl/homepage.html.twig', [
+            'title' => 'Symfony & 90\'s Jams',
+            'tracks' => $tracks
+        ]);
     }
 
     #[Route('/browse/{slug}')]
